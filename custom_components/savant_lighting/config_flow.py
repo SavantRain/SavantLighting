31cbdd2aa952dc0656bfd9d4f513c286b1910ad3
run.meng@savant.com.cn <run.meng@savant.com.cn>
2024-08-19 20:51:49 +0800

    modified:   custom_components/savant_lighting/config_flow.py 	custom_components/savant_lighting/translations/

--- custom_components/savant_lighting/config_flow.py
+++ custom_components/savant_lighting/config_flow.py
@@ -17,13 +17,41 @@ class SavantLightingConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 return await self.async_step_light_config()
             elif self.selected_type == "switch":
                 return await self.async_step_switch_config()
-
+            elif self.selected_type == "gateway":
+                return await self.async_step_gateway_config()
+            
         # Define the schema for the first step
         data_schema = vol.Schema({
             vol.Required("device_type", default="light"): vol.In(["light", "switch"]),
         })
 
-        return self.async_show_form(step_id="user", data_schema=data_schema)
+        return self.async_show_form(
+            step_id="user",
+            data_schema=data_schema
+        )
+
+    async def async_step_gateway_config(self, user_input=None):
+        """Handle the configuration for a gateway device."""
+        if user_input is not None:
+            # User has completed the gateway config step, create the entry
+            return self.async_create_entry(title=user_input["name"], data={
+                "type": "gateway",
+                "name": user_input["name"],
+                "host": user_input["host"],
+                "port": user_input["port"]
+            })
+
+        # Define the schema for the gateway configuration step
+        data_schema = vol.Schema({
+            vol.Required("name"): str,
+            vol.Required("host"): str,
+            vol.Required("port"): int,
+        })
+
+        return self.async_show_form(
+            step_id="gateway_config",
+            data_schema=data_schema
+        )
 
     async def async_step_light_config(self, user_input=None):
         """Handle the configuration for a light device."""
@@ -43,7 +71,10 @@ class SavantLightingConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             vol.Required("port"): int,
         })
 
-        return self.async_show_form(step_id="light_config", data_schema=data_schema)
+        return self.async_show_form(
+            step_id="light_config",
+            data_schema=data_schema
+        )
 
     async def async_step_switch_config(self, user_input=None):
         """Handle the configuration for a switch device."""
@@ -65,4 +96,7 @@ class SavantLightingConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             vol.Required("identifier"): int,
         })
 
-        return self.async_show_form(step_id="switch_config", data_schema=data_schema)
+        return self.async_show_form(
+            step_id="switch_config",
+            data_schema=data_schema
+        )
