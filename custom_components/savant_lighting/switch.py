dfee6f340a870427299c84d02759f96f81f83b16
SavantRain <run.meng@savant.com.cn>
2024-09-25 16:04:33 +0800

    modified:   custom_components/savant_lighting/switch.py

--- custom_components/savant_lighting/switch.py
+++ custom_components/savant_lighting/switch.py
@@ -148,6 +148,7 @@ class SavantSwitch(SwitchEntity):
                 s.sendall(data)
 #                response = s.recv(1024)
 #                return response
+
         except socket.error as e:
             _LOGGER.error(f"Error sending data to {self._host}:{self._port} - {e}")
             
